28c2d2d27cf101dddd6909978b022f9e9be33efa
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2022-09-30 16:46:47 -0400

    make my customized version without animations and better caption handling
    I originally was just looking to adjust it so the captions didn't show if the number of columns or rows was below the threshold where it didn't matter because nothing is hidden. Also found removing the animation part made it work in Jupyter. That was the main changes.

--- rich_dataframe/rich_dataframe.py
+++ rich_dataframe/rich_dataframe.py
@@ -1,29 +1,42 @@
+###--------------------CUSTOMIZED RICH-DATAFRAME--------------------------###
+# Based, almost directly, on https://github.com/khuyentran1401/rich-dataframe 
+# Building it in because it is small and hasn't been updated much in a while &
+# most importantly because I want to customize how it handles the captioning.
+# SPECIFIC CUSTOMIZATIONS:
+#-------------------------
+# I don't want the caption describing how many rows or cols shown unless number 
+# of rows or columns is larger than how many are shown.
+# Plus, adding note to install rich.
+# Plus, removing the animation to the 'beat' because it causes weird spacing
+# in Juputer and I had already turned the speed up so high because I wasn't
+# interested in the animated aspect the default rich-dataframe makes.
+# Plus, removed `_change_width()` section since I'm not seeing a difference
+# without it when not running animation.
+#-------------------------
+# Adding this in this section of my script so only need rich installed if
+# using command line. Use of the main function directly wouldn't necessarily
+# need the code handling printng the dataframe in the terminal and so best
+# rich not required in such cases so not asking user to install something
+# that isn't used.
 import time
-from contextlib import contextmanager
-
 import pandas as pd
+from contextlib import contextmanager
 from rich import print
+try:
+    from rich import print
+except ImportError:
+    sys.stderr.write("Run `pip install rich` on your command line "
+        "or if\nusing in a Jupyter notebook, run `%pip install rich`"
+        ", without the tick marks.\n**EXITING.**.\n")
+    sys.exit(1)
 from rich.box import MINIMAL, SIMPLE, SIMPLE_HEAD, SQUARE
 from rich.columns import Columns
 from rich.console import Console
 from rich.live import Live
 from rich.measure import Measurement
 from rich.table import Table
-
 console = Console()
-
-BEAT_TIME = 0.008
-
 COLORS = ["cyan", "magenta", "red", "green", "blue", "purple"]
-
-
-@contextmanager
-def beat(length: int = 1) -> None:
-    with console:
-        yield
-    time.sleep(length * BEAT_TIME)
-
-
 class DataFramePrettify:
     """Create animated and pretty Pandas DataFrame
 
@@ -44,7 +57,6 @@ class DataFramePrettify:
     clear_console: bool, optional
          Clear the console before printing the table, by default True. If this is set to False the previous console input/output is maintained
     """
-
     def __init__(
         self,
         df: pd.DataFrame,
@@ -67,7 +79,6 @@ class DataFramePrettify:
         self.col_limit = col_limit
         self.first_cols = first_cols
         self.clear_console = clear_console
-
         if first_cols:
             self.columns = self.df.columns[:col_limit]
         else:
@@ -78,76 +89,41 @@ class DataFramePrettify:
             self.rows = self.df.values[:row_limit]
         else:
             self.rows = self.df.values[-row_limit:]
-        
+
         if self.clear_console:
             console.clear()
 
     def _add_columns(self):
         for col in self.columns:
-            with beat(self.delay_time):
-                self.table.add_column(str(col))
-
+            self.table.add_column(str(col))
     def _add_rows(self):
         for row in self.rows:
-            with beat(self.delay_time):
-
-                if self.first_cols:
-                    row = row[: self.col_limit]
-                else:
-                    row = row[-self.col_limit :]
-
-                row = [str(item) for item in row]
-                self.table.add_row(*list(row))
-
+            if self.first_cols:
+                row = row[: self.col_limit]
+            else:
+                row = row[-self.col_limit :]
+            row = [str(item) for item in row]
+            self.table.add_row(*list(row))
     def _move_text_to_right(self):
         for i in range(len(self.table.columns)):
-            with beat(self.delay_time):
-                self.table.columns[i].justify = "right"
-
+            self.table.columns[i].justify = "right"
     def _add_random_color(self):
         for i in range(len(self.table.columns)):
-            with beat(self.delay_time):
-                self.table.columns[i].header_style = COLORS[
-                    i % self.num_colors
-                ]
-
+            self.table.columns[i].header_style = COLORS[
+                i % self.num_colors
+            ]
     def _add_style(self):
         for i in range(len(self.table.columns)):
-            with beat(self.delay_time):
-                self.table.columns[i].style = (
-                    "bold " + COLORS[i % self.num_colors]
-                )
-
+            self.table.columns[i].style = (
+                "bold " + COLORS[i % self.num_colors]
+            )
     def _adjust_box(self):
         for box in [SIMPLE_HEAD, SIMPLE, MINIMAL, SQUARE]:
-            with beat(self.delay_time):
-                self.table.box = box
-
+            self.table.box = box
     def _dim_row(self):
-        with beat(self.delay_time):
-            self.table.row_styles = ["none", "dim"]
-
+        self.table.row_styles = ["none", "dim"]
     def _adjust_border_color(self):
-        with beat(self.delay_time):
-            self.table.border_style = "bright_yellow"
-
-    def _change_width(self):
-        original_width = Measurement.get(console, self.table).maximum
-        width_ranges = [
-            [original_width, console.width, 2],
-            [console.width, original_width, -2],
-            [original_width, 90, -2],
-            [90, original_width + 1, 2],
-        ]
-
-        for width_range in width_ranges:
-            for width in range(*width_range):
-                with beat(self.delay_time):
-                    self.table.width = width
-
-            with beat(self.delay_time):
-                self.table.width = None
-
+        self.table.border_style = "bright_yellow"
     def _add_caption(self):
         if self.first_rows:
             row_text = "first"
@@ -158,12 +134,12 @@ class DataFramePrettify:
         else:
             col_text = "last"
 
-        with beat(self.delay_time):
-            self.table.caption = f"Only the {row_text} {self.row_limit} rows and the {col_text} {self.col_limit} columns is shown here."
-        with beat(self.delay_time):
-            self.table.caption = f"Only the [bold green] {row_text} {self.row_limit} rows[/bold green] and the [bold red]{self.col_limit} {col_text} columns[/bold red] is shown here."
-        with beat(self.delay_time):
+        if (len(self.df) > self.row_limit) and (len(self.df.columns) > self.col_limit):
             self.table.caption = f"Only the [bold magenta not dim] {row_text} {self.row_limit} rows[/bold magenta not dim] and the [bold green not dim]{col_text} {self.col_limit} columns[/bold green not dim] are shown here."
+        elif len(self.df) > self.row_limit:
+            self.table.caption = f"Only the [bold magenta not dim] {row_text} {self.row_limit} rows[/bold magenta not dim] are shown here."
+        elif len(self.df.columns) > self.col_limit:
+            self.table.caption = f"Only  the [bold green not dim]{col_text} {self.col_limit} columns[/bold green not dim] are shown here."
 
     def prettify(self):
         with Live(
@@ -179,10 +155,7 @@ class DataFramePrettify:
             self._add_style()
             self._adjust_border_color()
             self._add_caption()
-
         return self.table
-
-
 def prettify(
     df: pd.DataFrame,
     row_limit: int = 20,
@@ -219,3 +192,4 @@ def prettify(
     else:
         # In case users accidentally pass a non-datafame input, use rich's print instead
         print(df)
+###---------------END OF CUSTOMIZED RICH-DATAFRAME--------------------------###
